29dffd3013994ab19c4cbe1b087bf22ac5472092
jiawenjun <jiawenjun@ysstech.com>
2023-03-08 15:10:02 +0800

    create outing site

--- mysite/outing/apps.py
+++ mysite/outing/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class Outing123Config(AppConfig):
+class OutingConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "outing"
--- mysite/outing/views.py
+++ mysite/outing/views.py
@@ -1,5 +1,13 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 
-def index(request):
+def tpl(request):
+    context = {
+        'latest_question_list': 'ok',
+    }
+    return render(request, 'outing/index.html', context)
+
+
+def json(request):
     return HttpResponse("Hello, world. You're at the outing index.")
